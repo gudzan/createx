9612d41f2e69340d74e72c8d34edb97cd6e4d3a9
gudzan <andreeva.anva@gmail.com>
2024-07-01 16:31:19 +0300

    doc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function () {
+    var mixer = mixitup('.directions__list');
     $(".directions__filter-btn").on("click", function () {
         $(".directions__filter-btn").removeClass(
             "directions__filter-btn--active"
